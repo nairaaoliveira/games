405319cd6210a5e0d5892f71237002cd97a42a9d
weverson23 <weversonoliveira12@gmail.com>
2017-08-19 12:57:52 -0300

    atividade TEP

--- games/views.py
+++ games/views.py
@@ -44,67 +44,10 @@ def game_detail(request, pk):
 		return Response(game_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 	elif request.method == 'DELETE':
-		game.delete()
-		return Response(status=status.HTTP_204_NO_CONTENT)
-		return Response({"release_date": "This game is already launched!"}, status=status.HTTP_403_FORBIDDEN)
-
-
-@api_view(['GET', 'POST', 'PUT'])
-def game_detail(request, pk):
-	try:
-		game = Game.objects.get(pk=pk)
-	except Game.DoesNotExist:
-		return Response(status=status.HTTP_400_NOT_FOUND)
-
-	if request.method == 'GET':
-		game = Game.objects.all()
-		game_serializer = GameSerializer(games, many=True)
-		return Response(games_serializer.data)
-
-	elif request.method == 'POST':
-		game_serializer = GameSerializer(data=request.data)
-		if game_serializer.is_valid():
-			game_serializer.save()
-			return Response(game_serializer.data, status=status.HTTP_201_CREATED)
-		return Response(game_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-	elif request.method == 'PUT':
-		game_serializer = GameSerializer(game, data=request.data)
-		if game_serializer.is_valid():
-			game_serializer.save()
-			return Response(game_serializer.data)
-		return Response(game_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['GET', 'PUT', 'POST', 'DELETE'])
-def game_detail(request, pk):
-	try:
-		game = Game.objects.get(pk=pk)
-	except Game.DoesNotExist:
-		return Response(status=status.HTTP_400_NOT_FOUND)
-
-	if request.method == 'GET':
-		game_serializer = GameSerializer(game)
-		return Response(game_serializer.data)
-
-	elif request.method == 'PUT':
-		game_serializer = GameSerializer(game, data=request.data)
-		if game_serializer.is_valid():
-			game_serializer.save()
-			return Response(game_serializer.data)
-		return Response(game_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-	
-	elif request.method == 'POST':
-		game_serializer = GameSerializer(data=request.data)
-		if game_serializer.is_valid():
-			game_serializer.save()
-			return Response(game_serializer.data, status=status.HTTP_201_CREATED)
-		return Response(game_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-	elif request.method == 'DELETE':
-		game_serializer = GameSerializer(data=request.data)
-		if game_serializer.is_valid():
+		date = timezone.make_aware(datetime.now(), timezone.get_current_timezone())
+		if (date) < (game.release_date):
 			game.delete()
-			return Response(status=status.HTTP_204_NO_CONTENT)		
+			return Response(status=status.HTTP_204_NO_CONTENT)
+		return Response({"release_date": "Jogo não lancado. Nao pode ser excluido!"}, status=status.HTTP_403_FORBIDDEN)
+
 
